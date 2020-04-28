eda3c4748b414d5dc279f916d9f3dbc484524140
he.fanghui <he.fanghui@imprexion.com.cn>
2020-04-28 16:06:16 +0800

    chore: 添加测试调试项目objc-test

---
+++ objc4-test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc4-test
+//
+//  Created by 002 on 2020/4/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSObject *o = [[NSObject alloc] init];
+    }
+    return 0;
+}
