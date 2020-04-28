8165f732700bde3a50397b0ce73855fbc01c992c
he.fanghui <he.fanghui@imprexion.com.cn>
2020-04-28 16:03:28 +0800

    chore: 配置objc4的依赖文件和配置，完成objc4的正常编译

---
+++ objc-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  objc-test
+//
+//  Created by 002 on 2020/4/28.
+//
+
+#include <stdio.h>
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    NSObject *object = [[NSObject alloc] init];
+    return 0;
+}
